d2ff51ad6e9b307b627f1c9f374fcc2d329a8e31
Darshan Hegde <backup.darshanhegde@gmail.com>
2024-11-14 16:29:13 +0530

    updated

---
+++ inventory_backend/models/product.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const productSchema=new mongoose.Schema(
+{ 
+    Image:
+    {
+        type:String,
+        required:false,
+    },
+
+    productName:
+    {
+        type: String,
+        required:false,
+    },
+
+    BarcodeNo:
+    {
+        type: Number,
+        required:false,
+    },
+
+    Category:
+    {
+        type:Number,
+        required:false,
+
+    },
+
+    BuyingPrice:
+    {
+        type:Number,
+        required:false,
+
+    },
+
+    Quantity:
+    {
+        type: Number,
+        required:false,
+        default:0,
+    },
+
+    Unit:
+    {
+        type:Number,
+        required:false,
+        default:0,
+    
+    },
+
+    expiryDate:{
+        type:Date,
+        required:false,
+    },
+
+    thresholdValue:
+    {
+        type:Number,
+        required:false,
+    },
+},
+{
+    timestamps: true
+}
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
