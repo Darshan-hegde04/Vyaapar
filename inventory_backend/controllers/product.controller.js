a50834ad65e9b9f3284dae7b4c1c895861299b9e
Darshan Hegde <backup.darshanhegde@gmail.com>
2024-11-14 19:07:47 +0530

    Update product.controller.js

--- inventory_backend/controllers/product.controller.js
+++ inventory_backend/controllers/product.controller.js
@@ -12,7 +12,7 @@ const getProducts = async (req, res) => {
 const getProduct = async (req, res) => {
   try {
     const { id:productId } = req.params;
-    const product = await Product.findById(productId);
+    const product = await Product.findOne(productId);
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
